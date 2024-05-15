ae7f916fa5ecee5553d0eadd5f8d661d6aaa7067
trunglevie0319 <trunglevie0319@gmail.com>
2024-05-16 03:28:23 +0700

    Cập nhật Model cho Course

--- php/models/Course.php
+++ php/models/Course.php
@@ -33,12 +33,72 @@ class Course {
 
     $stmt->bind_param(":table", $this->table);
     $stmt->bind_param(":CourseID", $this->CourseID);
-    $stmt->bind_param("CourseName", $this->CourseName);
+    $stmt->bind_param(":CourseName", $this->CourseName);
     $stmt->bind_param(":Progress_Ratio", $this->Progress_Ratio);
     $stmt->bind_param(":Midterm_Ratio", $this->Midterm_Ratio);
     $stmt->bind_param(":Finaltest_Ratio", $this->Finaltest_Ratio);   
     
     if ($stmt->execute()) return $stmt->affected_rows > 0;
     else return false;
+  }
+
+  public function getAll(): mysqli_result {
+    $sql = "SELECT * FROM :table";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(":table", $this->table);
+    $stmt->execute();
+    return $stmt->get_result();
+  }
+
+  public function getOne($id): mysqli_result {
+    $id = preprocess_input($id);
+    $sql = "SELECT * FROM :table WHERE CourseID=:CourseID LIMIT 1";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(":table", $this->table);
+    $stmt->bind_param(":CourseID", $id);
+    $stmt->execute();
+    return $stmt->get_result();
+  }
+
+  public function find($str): mysqli_result {
+    $str = preprocess_input($str);
+    $sql = "SELECT * FROM :table WHERE CourseID LIKE %:str% OR CourseName LIKE %:str%";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(":table", $this->table);
+    $stmt->bind_param(":str", $str);
+    $stmt->execute();
+    return $stmt->get_result();
+  }
+
+  public function update(): bool {
+    $this->CourseID = preprocess_input($this->CourseID);
+    $this->CourseName = preprocess_input($this->CourseName);
+    $this->Progress_Ratio = preprocess_input($this->Progress_Ratio);
+    $this->Midterm_Ratio = preprocess_input($this->Midterm_Ratio);
+    $this->Finaltest_Ratio = preprocess_input($this->Finaltest_Ratio);
+
+    $sql = "UPDATE :table SET CourseName=:CourseName, 
+            Progress_Ratio=:Progress_Ratio, Midterm_Ratio=:Midterm_Ratio, 
+            Finaltest_Ratio=:Finaltest_Ratio WHERE CourseID=:CourseID";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(":table", $this->table);
+    $stmt->bind_param(":CourseID", $this->CourseID);
+    $stmt->bind_param(":CourseName", $this->CourseName);
+    $stmt->bind_param(":Progress_Ratio", $this->Progress_Ratio);
+    $stmt->bind_param(":Midterm_Ratio", $this->Midterm_Ratio);
+    $stmt->bind_param(":Finaltest_Ratio", $this->Finaltest_Ratio);
+
+    if ($stmt->execute()) return $stmt->affected_rows;
+    return false;
+  }
+
+  public function delete($id): bool {
+    $id = preprocess_input($id);
+    $sql = "DELETE FROM :table WHERE CourseID=:CourseID";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bind_param(":table", $this->table);
+    $stmt->bind_param(":CourseID", $this->CourseID);
+    if ($stmt->execute()) return $stmt->affected_rows;
+    return false;
   }
 }
